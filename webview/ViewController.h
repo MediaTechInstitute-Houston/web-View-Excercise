f115159b799c454efc602b6bddd19c476ae61b7f
dumesm1 <markdumes@Marks-MacBook-Air.local>
2014-02-14 12:22:23 -0600

    Initial Commit

---
+++ webview/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  webview
+//
+//  Created by Mark Dumes on 2/14/14.
+//  Copyright (c) 2014 CollegeBlink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ webviewTests/webviewTests.m
@@ -0,0 +1,34 @@
+//
+//  webviewTests.m
+//  webviewTests
+//
+//  Created by Mark Dumes on 2/14/14.
+//  Copyright (c) 2014 CollegeBlink. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface webviewTests : XCTestCase
+
+@end
+
+@implementation webviewTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
